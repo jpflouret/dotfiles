d02e8fcbd5fb3e75cc50786d0e9d1703fe09e3fc
JP Flouret <jpflouret@gmail.com>
2014-11-12 15:57:49 -0800

    Added mklinks.sh for installation

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # dotfiles
 
 Nothing to see here. Move along.
+
+# Install
+
+    cd ~
+    git clone --recursive git@github.com:jpflouret/dotfiles.git
+    ./dotfiles/mklinks.sh -f
+
+or with https (if ssh key hasn't been set yet)
+
+    cd ~
+    git clone --recursive https://github.com/jpflouret/dotfiles.git
+    ./dotfiles/mklinks.sh -f
---
+++ mklinks.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+DST=$HOME
+DOTFILES=`readlink -en \`dirname $0\``
+
+if [ -z "$CYGWIN" ]; then
+  export CYGWIN=winsymlinks:native
+fi
+
+
+lowercase(){
+    echo "$1" | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/"
+}
+
+show_help() {
+  echo "$0 [-f] [-h]"
+  echo "  -f    Force overwrite"
+  echo "  -h    Show this help"
+}
+
+
+force=0
+while getopts "h?f" opt; do
+    case "$opt" in
+    h|\?)
+        show_help
+        exit 0
+        ;;
+    f)  force=1
+        ;;
+    esac
+done
+
+
+LN="ln -s"
+[ $force -eq 0 ] || LN="ln -sf"
+
+SYSNAME=`lowercase \`uname\``
+case "$SYSNAME" in
+  cygwin*)  SYSNAME=cygwin ;;
+esac
+
+
+[ -d $DST ] || mkdir $DST
+
+# Special case for vimfiles since it is a whole dir
+if [ -d $DOTFILES/vimfiles ]; then
+  $LN -T $DOTFILES/vimfiles $DST/.vim
+  # On cygwin also create vimfiles in the user profile dir
+  if [ $SYSNAME = "cygwin" ]; then
+    $LN -T $DOTFILES/vimfiles $(cygpath $USERPROFILE)/vimfiles
+  fi
+fi
+
+for file in `find $DOTFILES -maxdepth 1 -type f \( -iname "*rc" -o -iname "*.conf" -o -iname "*profile" -o -iname "*config" \) -print`; do
+  target=`basename $file`
+  $LN $file $DST/.$target
+done
+
+[ -d $DST/bin ] || mkdir $DST/bin
+for file in `find $DOTFILES/bin -maxdepth 1 -type f -print`; do
+  target=`basename $file`
+  $LN $file $DST/bin/$target
+done
+
+if [ -d $DOTFILES/bin/$sysname ]; then
+  for file in `find $DOTFILES/bin/$SYSNAME -type f -print`; do
+    target=`basename $file`
+    $LN $file $DST/bin/$target
+  done
+fi
+
+# vim:ts=2:sw=2:et:tw=0:
